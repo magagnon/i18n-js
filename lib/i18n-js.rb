8c6121eb0e8778614dc4cb6b81055c36f453f961
Marc-Andre Gagnon <magagnon@carretechnologies.com>
2014-02-18 19:09:52 -0500

    Added test for rails 4 in assert_usable_configuration! - Rails 4 doesnt use initialize_on_precompile

--- lib/i18n-js.rb
+++ lib/i18n-js.rb
@@ -15,9 +15,12 @@ module SimplesIdeias
     # full environment will be available during asset compilation.
     # This is required to ensure I18n is loaded.
     def assert_usable_configuration!
-      @usable_configuration ||= Rails.version >= "3.1.1" &&
+      @usable_configuration ||= if Rails.version >= "3.1.1" && Rails.version < "4.0.0"
         Rails.configuration.assets.initialize_on_precompile ||
         raise("Cannot precompile i18n-js translations unless environment is initialized. Please set config.assets.initialize_on_precompile to true.")
+      else
+        true
+      end
     end
 
     def has_asset_pipeline?
